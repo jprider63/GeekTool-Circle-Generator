57862900067222f63373515727f1dc05f7e51d97
jp.rider63 <dev@jamesparker.me>
2013-08-01 00:06:26 -0400

    Initial Commit

---
+++ Geeklet Images/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Geeklet Images
+//
+//  Created by James Parker on 8/1/13.
+//  Copyright (c) 2013 university of md college park. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
