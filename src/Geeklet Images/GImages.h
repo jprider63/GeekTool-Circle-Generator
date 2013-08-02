53054d4b4fe9a6b64f09075a338795544e74e616
jp.rider63 <dev@jamesparker.me>
2013-08-02 00:52:27 -0400

    Added Circles.zip, which contains the output circles of this script.

---
+++ src/Geeklet Images/GImages.h
@@ -0,0 +1,13 @@
+//
+//  GImages.h
+//  Geeklet Images
+//
+//  Created by James Parker on 8/1/13.
+//  Copyright (c) 2013 university of md college park. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GImages : NSObject
+
+@end
---
+++ src/Geeklet Images/GImages.m
@@ -0,0 +1,13 @@
+//
+//  GImages.m
+//  Geeklet Images
+//
+//  Created by James Parker on 8/1/13.
+//  Copyright (c) 2013 university of md college park. All rights reserved.
+//
+
+#import "GImages.h"
+
+@implementation GImages
+
+@end
